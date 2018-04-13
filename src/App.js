f5bbbe66a11f6e7659e045b548306fd2074392f6
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2018-04-13 13:23:10 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,188 @@
+import React, { Component } from 'react';
+import {HashRouter, Route, NavLink, Switch, Link} from 'react-router-dom';
+import './App.css';
+import defaultState from './defaultState.js'
+import queryString from 'query-string'
+
+import {Animated} from "react-animated-css";
+
+
+
+class App extends Component {
+  state = defaultState
+
+  render() {
+    return (
+        <HashRouter hashType="noslash">
+          <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Lombard In America</h1>
+
+            <NavLink className="navlink" exact activeClassName="is-active" to="/">Home</NavLink>
+            <NavLink className="navlink" exact activeClassName="is-active" to="/about">About</NavLink>
+            <NavLink className="navlink" exact activeClassName="is-active" to="/repositories">Repositories</NavLink>
+            <NavLink className="navlink" exact activeClassName="is-active" to="/manuscripts">Manuscripts</NavLink>
+
+          </header>
+          <Switch>
+            <Route exact path="/" component={Home}>
+            </Route>
+            <Route exact path="/about" component={About}>
+            </Route>
+            <Route exact path="/repositories" component={Repositories}>
+            </Route>
+            <Route exact path="/repositories/:id" component={RepoDescription}>
+            </Route>
+            <Route exact path="/manuscripts" component={Manuscripts}>
+            </Route>
+            <Route exact path="/manuscripts/:id" component={MsDescription}>
+            </Route>
+
+          </Switch>
+          </div>
+        </HashRouter>
+    );
+  }
+}
+
+
+class About extends Component {
+  render() {
+    return (
+      <p>About</p>
+    );
+  }
+}
+class Home extends Component {
+  render() {
+    return (
+      <p>Home</p>
+    );
+  }
+}
+class Repositories extends Component {
+  render() {
+
+    const displayRepos = () => {
+      const repos = defaultState.repos.map((repo, i) => {
+      const path = "/repositories/" + i
+        return <Link key={repo.id} to={path}><Repository repo={repo} index={i}/></Link>
+      });
+      return repos
+    }
+    return (
+      <div className="repo-list">
+      {displayRepos()}
+      </div>
+    );
+  }
+}
+class RepoDescription extends Component {
+  render() {
+    const index = this.props.match.params.id
+    const msPath = "/manuscripts?repo=" + defaultState.repos[index].id
+    return (
+      <Animated animationIn="bounceInRight" animationOut="fadeOut" isVisible={true}>
+        <div>
+          <p>
+            <Link to="/repositories">Back to Repos</Link>
+          </p>
+          {defaultState.repos[index].title && <p>{defaultState.repos[index].title}</p>}
+          <p><Link to={msPath}>View Repo Mss</Link></p>
+        </div>
+
+      </Animated>
+
+    );
+  }
+}
+class Repository extends Component {
+  state = {
+    isVisible: true
+  }
+  componentWillUnmount(){
+    this.setState(() => {
+      return {
+        isVisible: false
+      }
+    })
+  }
+  render() {
+    return (
+      <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={this.state.isVisible} animationInDelay={0}>
+          <p>{this.props.repo.title}</p>
+      </Animated>
+
+    );
+  }
+}
+
+class Manuscripts extends Component {
+  render() {
+    const filter = queryString.parse(this.props.location.search).repo;
+
+    const displayMss = () => {
+      const mss = defaultState.mss.map((ms, i) => {
+        const path = "/manuscripts/" + i
+        if (filter){
+          if (filter === ms.repo ){
+            return <Manuscript key={ms.id} ms={ms} index={i} path={path}/>
+          }
+        }
+        else {
+          return <Manuscript key={ms.id} ms={ms} index={i} path={path}/>
+        }
+      });
+      return mss
+    }
+    const index = defaultState.repos.findIndex(r => r.id === filter);
+    const backPath = "/repositories/" + index;
+
+    return (
+      <div className="ms-list">
+        {filter && <p><Link to={backPath}>Back to Repo Description</Link></p>}
+        {displayMss()}
+      </div>
+    );
+  }
+}
+class MsDescription extends Component {
+  render() {
+
+    const displayDescription = () => {
+      const index = this.props.match.params.id;
+      console.log(index)
+      const backPath = "/manuscripts?repo=" + defaultState.mss[index].repo
+      if (index){
+        return(
+          <div>
+            <p>
+              <Link to={backPath}>Back to Mss. for this Repo</Link>
+            </p>
+            {defaultState.mss[index].title && <p>{defaultState.mss[index].title}</p>}
+            {defaultState.mss[index].description && <p>{defaultState.mss[index].description}</p>}
+          </div>
+        )
+      }
+    }
+    return (
+      <Animated animationIn="bounceInRight" animationOut="bounceOutLeft" isVisible={true}>
+        {displayDescription()}
+      </Animated>
+
+    );
+  }
+}
+class Manuscript extends Component {
+  render() {
+    return (
+      <Animated animationIn="bounceInLeft" animationOut="bounceOutLeft" isVisible={this.props.isVisible} animationInDelay={0}>
+          <p><Link to={this.props.path}>{this.props.ms.title}</Link></p>
+      </Animated>
+
+    );
+  }
+}
+
+
+export default App;
---
+++ src/defaultState.js
@@ -0,0 +1,206 @@
+const defaultState = {
+  repos: [
+    {
+      id: "walters",
+      title: "Walters"
+    },
+    {
+      id: "hopkins",
+      title: "Hopkins"
+    },
+    {
+      id: "upenn",
+      title: "University of Pennsylvania"
+    },
+    {
+      id: "freelibrary",
+      title: "Free Library of Philadelphia"
+    },
+    {
+      id: "yale",
+      title: "Yale"
+    },
+    {
+      id: "harvard",
+      title: "Harvard"
+    },
+    {
+      id: "newberry",
+      title: "Newberry"
+    },
+    {
+      id: "marymount",
+      title: "Loyola Marymount University"
+    }
+  ],
+  mss: [
+    {
+      id: "harv245",
+      title: "Harvard 245",
+      repo: "harvard"
+    },
+    {
+      id: "harv265",
+      title: "Harvard 265",
+      repo: "harvard"
+    },
+    {
+      id: "hopkinsMSB19",
+      title: "Hopkins Ms B19",
+      repo: "hopkins"
+    },
+    {
+      id: "penn",
+      title: "Penn",
+      repo: "upenn",
+      description: "penn 686, rothwell text"
+    },
+    {
+      id: "penn727",
+      title: "Penn 727",
+      repo: "upenn",
+      description: "anonymous augustinian commentary"
+    },
+    {
+      id: "penn1147",
+      title: "Penn 1147",
+      repo: "upenn",
+      description: "book III of lombard"
+    },
+    {
+      id: "pennColl591",
+      title: "Penn Coll 591",
+      repo: "upenn",
+      description: "fragment from Lombard book IV (2 leaves)"
+    },
+    {
+      id: "penn78",
+      title: "Penn 78",
+      repo: "upenn",
+      description: "Registry or list of distinction in lombard with lots of interesting marginalia"
+    },
+    {
+      id: "walt349",
+      title: "Walter 349",
+      repo: "walters"
+    },
+    {
+      id: "walt809",
+      title: "Walter 809",
+      repo: "walters"
+    },
+    {
+      id: "pfllewisE141",
+      title: "Free Library PFL Lewis E141",
+      repo: "freelibrary"
+    },
+    {
+      id: "pfllewisE170",
+      title: "Free Library PFL Lewis E170",
+      repo: "freelibrary"
+    },
+    {
+      id: "beinecke1207",
+      title: "beinecke 1207",
+      repo: "yale"
+    },
+    {
+      id: "beinecke619",
+      title: "beinecke 619",
+      repo: "yale"
+    },
+    {
+      id: "beinecke207",
+      title: "beinecke 207",
+      repo: "yale"
+    },
+    {
+      id: "beinecke20",
+      title: "beinecke 20",
+      repo: "yale"
+    },
+    {
+      id: "marston175",
+      title: "marston 175",
+      repo: "yale"
+    },
+    {
+      id: "marston222",
+      title: "marston 222",
+      repo: "yale"
+    },
+    {
+      id: "marston270",
+      title: "marston 270",
+      repo: "yale"
+    },
+    {
+      id: "marston893",
+      title: "marston893",
+      repo: "yale"
+    },
+    {
+      id: "beinecke712.130",
+      title: "beinecke712.130",
+      repo: "yale"
+    },
+    {
+      id: "beinecke712.131",
+      title: "beinecke712.131",
+      repo: "yale"
+    },
+    {
+      id: "newberry",
+      title: "beinecke712.131",
+      repo: "yale"
+    },
+    {
+      id: "case121",
+      title: "case 121",
+      repo: "newberry",
+      description: "fragment from book 3, 25 leaves, of Thomas Aquinas' commentary on the Sentences"
+    },
+    {
+      id: "case184",
+      title: "case 184",
+      repo: "newberry",
+      description: "fragment from book 4 of thomas aquinas"
+    },
+    {
+      id: "case20",
+      title: "case 20",
+      repo: "newberry",
+      description: "Lombard Sentences"
+    },
+    {
+      id: "case217",
+      title: "case 217",
+      repo: "newberry",
+      description: "compilation that includes fol. 9v-57r. Collationes super sententias Petri Lombardi."
+    },
+    {
+      id: "case72",
+      title: "case 72",
+      repo: "newberry",
+      description: "Commentary of Francis Mayronis"
+    },
+    {
+      id: "case24.5",
+      title: "case 24.5",
+      repo: "newberry",
+      description: "Commentary on book II by the Franciscan Simon de Lenis"
+    },
+    {
+      id: "marymount25",
+      title: "marymount 25",
+      repo: "marymount",
+      description: "Single leaf containing part of l4d11c1, all of b4d11c2, and part of b4d11c3"
+    }
+
+
+
+
+  ]
+}
+
+export default defaultState;
